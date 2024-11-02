3d9c31f41b51a60dfc479c4d390fb2d4859702c3
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2024-11-01 22:52:37 -0500

    Prueba Saber Pro-Version 1

--- envs/my_blog/posts/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from posts.models import Post
-
-
-class PostSerializer(ModelSerializer):
-    class Meta:
-        model=Post
-        #fileds='__all__'
-        fields=['title','description','order','created_at']
--- envs/my_blog/posts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    title=models.CharField(max_length=255)
-    description=models.TextField()
-    order=models.IntegerField(default=0)
-    created_at=models.DateTimeField(auto_now_add=True)
